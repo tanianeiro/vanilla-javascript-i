090c9f5ab0ab951200e1e5621e038b79ad29dd10
tania <fdfdfd@ddfdfj.es>
2021-11-02 09:40:36 +0100

    Inicializado ejercicio js
    -Creado js. Realizados ejer para alumnos

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+"use strict"
+
+//1 variable con nombre
+let nombre = "Tania";
+console.log(nombre);
+
+//2 cambio valor variable
+nombre = "numeros";
+console.log(nombre);
+
+//3 cuando se comenta la variable, la consola indica que dicha variable no está definida. 
+// Ningun valor definido.
+
+
+//4 creo tres variables, sumar y guardo el resultado.
+// puedo poner todas la variables en una misma fila. let operador1, operador2, resultado(iniciarlo en 0)
+let operador1 = 4;
+let operador2 = 5;
+let resultado = 0;
+resultado = operador1 + operador2
+console.log(resultado);
+
+
+// Ejercicios de  Profesor
+// Ejer 1. Algoritmo de programa que calcule área de círculo dado radio inicializado en una variable.
+// utilizar una constante PI. Console.log
+
+
+
